a842e854c603757a0f084b14acd17c3803636fa6
qiangangucd <qiangangzhu@gmail.com>
2019-10-09 22:38:13 +0100

    pdf

--- practical_4/main.py
+++ practical_4/main.py
@@ -101,42 +101,44 @@ def ent(data):
 
 
 if __name__ == '__main__':
-    # text0 = 'Today we went to the NLP course and learned how to segment words, how to extract stems and preprocess ' \
-    #         'the text. '
-    # text1 = 'today they are going to school and want to participant the text analyse course. It sounds very' \
-    #         ' nice due to the segment words'
-    # text2 = 'Yesterday they went to a data science course and learnt how to preprocess the text. they are very happy ' \
-    #         'due to the teacher was very nice '
-    # text3 = 'today\'s nlp course is very interesting, we have learnt a lot of knowledge and it is very useful for our ' \
-    #         'careers, so we are very happy '
-    # text4 = 'today they are going to school and want to participant the data mining course. It sounds very' \
-    #         ' nice due to the secret'
-    # text5 = 'the machine learning course is also very nice, because the teacher is very pleased and professional, ' \
-    #         'he teaches us some useful technology. '
-    # text6 = 'the weather is very terrible, resulting in he missed his bus and was late for school. As a result, he ' \
-    #         'missed the text analyse course. '
-    # text7 = 'The teacher taught us how to process the raw data and how to write the code to implement the algorithm ' \
-    #         'and how to evaluate the machine learning algorithm '
-    # text8 = 'today\'s weather is very nice, so they did not go to school and absence of machine learning course. They ' \
-    #         'just climbed the mountain and have a picnic '
-    # text9 = 'today they went to the gym and do some sports after they finish their homework assigned in machine data ' \
-    #         'mining course. They feel better through exercising. '
-    # # word_tokens0 = get_tokens(text0)
-    # word_tokens1 = get_tokens(text1)
-    # word_tokens2 = get_tokens(text2)
-    # word_tokens3 = get_tokens(text3)
-    # word_tokens4 = get_tokens(text4)
-    # word_tokens5 = get_tokens(text5)
-    # word_tokens6 = get_tokens(text6)
-    # word_tokens7 = get_tokens(text7)
-    # word_tokens8 = get_tokens(text8)
-    # word_tokens9 = get_tokens(text9)
+    text0 = 'Today we went to the NLP course and learned how to segment words, how to extract stems and preprocess ' \
+            'the text. '
+    text1 = 'today they are going to school and want to participant the text analyse course. It sounds very' \
+            ' nice due to the segment words'
+    text2 = 'Yesterday they went to a data science course and learnt how to preprocess the text. they are very happy ' \
+            'due to the teacher was very nice '
+    text3 = 'today\'s nlp course is very interesting, we have learnt a lot of knowledge and it is very useful for our ' \
+            'careers, so we are very happy '
+    text4 = 'today they are going to school and want to participant the data mining course. It sounds very' \
+            ' nice due to the secret'
+    text5 = 'the machine learning course is also very nice, because the teacher is very pleased and professional, ' \
+            'he teaches us some useful technology. '
+    text6 = 'the weather is very terrible, resulting in he missed his bus and was late for school. As a result, he ' \
+            'missed the text analyse course. '
+    text7 = 'The teacher taught us how to process the raw data and how to write the code to implement the algorithm ' \
+            'and how to evaluate the machine learning algorithm '
+    text8 = 'today\'s weather is very nice, so they did not go to school and absence of machine learning course. They ' \
+            'just climbed the mountain and have a picnic '
+    text9 = 'today they went to the gym and do some sports after they finish their homework assigned in machine data ' \
+            'mining course. They feel better through exercising. '
+    word_tokens0 = get_tokens(text0)
+    word_tokens1 = get_tokens(text1)
+    word_tokens2 = get_tokens(text2)
+    word_tokens3 = get_tokens(text3)
+    word_tokens4 = get_tokens(text4)
+    word_tokens5 = get_tokens(text5)
+    word_tokens6 = get_tokens(text6)
+    word_tokens7 = get_tokens(text7)
+    word_tokens8 = get_tokens(text8)
+    word_tokens9 = get_tokens(text9)
     # countlist_count = [word_tokens0[1], word_tokens1[1], word_tokens2[1], word_tokens3[1], word_tokens4[1], word_tokens5[1],
     #              word_tokens6[1],
     #              word_tokens7[1], word_tokens8[1], word_tokens9[1]]
-    # countlist_word = [word_tokens0[0], word_tokens1[0], word_tokens2[0], word_tokens3[0], word_tokens4[0], word_tokens5[0],
-    #              word_tokens6[0],
-    #              word_tokens7[0], word_tokens8[0], word_tokens9[0]]
+    countlist_word = [word_tokens0[0], word_tokens1[0], word_tokens2[0], word_tokens3[0], word_tokens4[0],
+                      word_tokens5[0],
+                      word_tokens6[0],
+                      word_tokens7[0], word_tokens8[0], word_tokens9[0]]
+    print(countlist_word)
     # compute_tfidf(countlist)
     # print(word_tokens0[1])
     # print(word_tokens0[1].elements())
@@ -178,43 +180,43 @@ if __name__ == '__main__':
     # spam_set.append('Find your perfect Ginger shampoo with Hair Growth')
     # spam_set.append('This Ginger shampoo allegedly makes great shampoo.')
     # spam_set.append('Thanks to Ginger, our Ginger  Shampoo and Conditioner not only refreshes')
-    spam_set.append('Let the Cameras In - Stop secret heath care negotiations')
-    spam_set.append('Stop secret heath care negotiations. Sign the petition to let the Cameras In!')
-    spam_set.append('Let the Cameras In - Stop secret heath care negotiations')
-    spam_set.append('Sign the petition - Let the Cameras In!')
-    spam_set.append('What are Speaker Pelosi,Sen. Reid and Pres Obama hiding?')
-    spam_set.append('Sign the petition - Let the Cameras In!')
-    spam_set.append('Sign the petition - Let the Cameras In!')
-    spam_set.append('No Secret health care negotiations!')
-    spam_set.append('No more secret health care negotiations! Tranparency now.')
-    spam_set.append('Let the Cameras In - Stop the secret health care negotiations')
-    # --------------------------------------------------------------------------------------------------------------------
-    random_set = []
-    random_set.append('This is a nice diagram by Zhengyan Zhang and @BakserWang that shows how many recent pretrained '
-                      'language models are connected. The GitHub repo contains a full list of relevant papers')
-    random_set.append(
-        'Working in partnership with @hotpress to curate a list of extraordinary tracks to inspire you in '
-        'the lead up to World Mental Health Day. Listen now on Spotify.')
-    random_set.append('A Vardy household source tells me that Colleen\'s message could actually put Rebekah in a '
-                      'stronger position')
-    random_set.append('Imagining the Vardy household right now, Rebekah shrieking blue murder, Jamie\'s on his ninth '
-                      'Red Bull of the morning, kids are crying')
-    random_set.append('Leo Varadkar has reneged on a secret deal with Boris Johnson to open the way to a Brexit '
-                      'compromise, a senior Downing Street source claimed yesterday')
-    random_set.append('It was touch and go but Wales have won it! Full-time Wales 29-17 Fiji')
-    random_set.append('It\'s up to you today to start making healthy choices. Not choices that are just healthy for '
-                      'your body, but healthy for your mind.')
-    random_set.append('I\'ve already perfect the art of fake smiling.')
-    random_set.append('VIDEO: Police and demonstrators clash outside Ecuador\'s National Assembly building as protests '
-                      'over a fuel hike introduced by President Lenin Moreno\'s government intensify')
-    random_set.append('How are wind farms installed in the sea?')
-
-    print(ent(pd.Series(['a', 'a', 'b', 'a'])))
-    print(spam_set)
-    print(random_set)
-    print('Entropy values for spam-set=', ent(pd.Series(spam_set)))
-    print('Entropy values for random-set=', ent(pd.Series(random_set)))
-    print('Entropy values for mixed-set=', ent(pd.Series(random_set + spam_set)))
-    print('Entropy values for [\'a\',\'b\',\'a\',\'c\',\'b\',\'a\']=',ent(pd.Series(['a','b','a','c','b','a'])))
-    print('Entropy values for [\'a\',\'a\',\'a\',\'a\',\'a\',\'a\']=', ent(pd.Series(['a', 'a', 'a', 'a', 'a', 'a'])))
-    print('Entropy values for [\'a\',\'b\',\'c\',\'d\',\'e\',\'f\']=', ent(pd.Series(['a', 'b', 'c', 'd', 'e', 'f'])))
+    # spam_set.append('Let the Cameras In - Stop secret heath care negotiations')
+    # spam_set.append('Stop secret heath care negotiations. Sign the petition to let the Cameras In!')
+    # spam_set.append('Let the Cameras In - Stop secret heath care negotiations')
+    # spam_set.append('Sign the petition - Let the Cameras In!')
+    # spam_set.append('What are Speaker Pelosi,Sen. Reid and Pres Obama hiding?')
+    # spam_set.append('Sign the petition - Let the Cameras In!')
+    # spam_set.append('Sign the petition - Let the Cameras In!')
+    # spam_set.append('No Secret health care negotiations!')
+    # spam_set.append('No more secret health care negotiations! Tranparency now.')
+    # spam_set.append('Let the Cameras In - Stop the secret health care negotiations')
+    # # --------------------------------------------------------------------------------------------------------------------
+    # random_set = []
+    # random_set.append('This is a nice diagram by Zhengyan Zhang and @BakserWang that shows how many recent pretrained '
+    #                   'language models are connected. The GitHub repo contains a full list of relevant papers')
+    # random_set.append(
+    #     'Working in partnership with @hotpress to curate a list of extraordinary tracks to inspire you in '
+    #     'the lead up to World Mental Health Day. Listen now on Spotify.')
+    # random_set.append('A Vardy household source tells me that Colleen\'s message could actually put Rebekah in a '
+    #                   'stronger position')
+    # random_set.append('Imagining the Vardy household right now, Rebekah shrieking blue murder, Jamie\'s on his ninth '
+    #                   'Red Bull of the morning, kids are crying')
+    # random_set.append('Leo Varadkar has reneged on a secret deal with Boris Johnson to open the way to a Brexit '
+    #                   'compromise, a senior Downing Street source claimed yesterday')
+    # random_set.append('It was touch and go but Wales have won it! Full-time Wales 29-17 Fiji')
+    # random_set.append('It\'s up to you today to start making healthy choices. Not choices that are just healthy for '
+    #                   'your body, but healthy for your mind.')
+    # random_set.append('I\'ve already perfect the art of fake smiling.')
+    # random_set.append('VIDEO: Police and demonstrators clash outside Ecuador\'s National Assembly building as protests '
+    #                   'over a fuel hike introduced by President Lenin Moreno\'s government intensify')
+    # random_set.append('How are wind farms installed in the sea?')
+    #
+    # print(ent(pd.Series(['a', 'a', 'b', 'a'])))
+    # print(spam_set)
+    # print(random_set)
+    # print('Entropy values for spam-set=', ent(pd.Series(spam_set)))
+    # print('Entropy values for random-set=', ent(pd.Series(random_set)))
+    # print('Entropy values for mixed-set=', ent(pd.Series(random_set + spam_set)))
+    # print('Entropy values for [\'a\',\'b\',\'a\',\'c\',\'b\',\'a\']=',ent(pd.Series(['a','b','a','c','b','a'])))
+    # print('Entropy values for [\'a\',\'a\',\'a\',\'a\',\'a\',\'a\']=', ent(pd.Series(['a', 'a', 'a', 'a', 'a', 'a'])))
+    # print('Entropy values for [\'a\',\'b\',\'c\',\'d\',\'e\',\'f\']=', ent(pd.Series(['a', 'b', 'c', 'd', 'e', 'f'])))
---
+++ practical_4/tfidf_tookit.py
@@ -0,0 +1,80 @@
+import sys
+from nltk.tokenize import word_tokenize
+from nltk.stem.porter import PorterStemmer
+from nltk.corpus import stopwords
+from gensim.corpora import Dictionary
+from gensim.models import TfidfModel
+from wordcloud import WordCloud
+
+# stemmer = PorterStemmer() # Stemmer for reducing terms to root form
+#
+# stemmed_corpus = []       # For storing the stemmed tokens
+#
+# original_corpus = []      # For storing the non-stemmed tokens
+
+
+# for file in sys.argv[1:]:                # Iterate over the files
+#
+#     contents = open(file).read().lower() # Load file contents
+#
+#     tokens = word_tokenize(contents)     # Extract tokens
+#
+#     stemmed = [stemmer.stem(token) for token in tokens] # Stem tokens
+#
+#
+#     stemmed_corpus.append(stemmed)    # Store stemmed document
+#
+#     original_corpus.append(tokens)    # Store original document
+
+stemmed_corpus = [['today', 'go', 'nlp', 'course', 'learn', 'segment', 'word', 'extract', 'stem', 'preprocess', 'text'],
+                  ['today', 'go', 'school', 'want', 'participant', 'text', 'analyse', 'course', 'sound', 'nice', 'due',
+                   'segment', 'word'],
+                  ['yesterday', 'go', 'data', 'science', 'course', 'learnt', 'preprocess', 'text', 'happy', 'due',
+                   'teacher', 'nice'],
+                  ['today', 'nlp', 'course', 'interesting', 'learnt', 'lot', 'knowledge', 'useful', 'career', 'happy'],
+                  ['today', 'go', 'school', 'want', 'participant', 'data', 'mining', 'course', 'sound', 'nice', 'due',
+                   'secret'],
+                  ['machine', 'learn', 'course', 'also', 'nice', 'teacher', 'pleased', 'professional', 'teach', 'u',
+                   'useful', 'technology'],
+                  ['weather', 'terrible', 'result', 'missed', 'bus', 'late', 'school', 'result', 'miss', 'text',
+                   'analyse', 'course'],
+                  ['teacher', 'taught', 'u', 'process', 'raw', 'data', 'write', 'code', 'implement', 'algorithm',
+                   'evaluate', 'machine', 'learning', 'algorithm'],
+                  ['today', 'weather', 'nice', 'go', 'school', 'absence', 'machine', 'learn', 'course', 'climb',
+                   'mountain', 'picnic'],
+                  ['today', 'go', 'gym', 'sport', 'finish', 'homework', 'assign', 'machine', 'data', 'mining', 'course',
+                   'feel', 'well', 'exercise']]
+dictionary = Dictionary(stemmed_corpus)  # Build the dictionary
+
+# Convert to vector corpus
+
+vectors = [dictionary.doc2bow(text) for text in stemmed_corpus]
+
+# Build TF-IDF model
+
+tfidf = TfidfModel(vectors)
+
+# Get TF-IDF weights
+
+weights = tfidf[vectors[0]]
+# Get terms from the dictionary and pair with weights
+
+weights = dict((dictionary[pair[0]], pair[1]) for pair in weights)
+
+# Initialize the word cloud
+
+wc = WordCloud(
+    background_color="white",
+    # max_words=2000,
+    width=500,
+    height=500,
+    stopwords=stopwords.words("english")
+)
+
+# Generate the cloud
+
+wc.generate_from_frequencies(weights)
+
+# Save the could to a file
+
+wc.to_file("word_cloud.png")
